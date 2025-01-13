71092246f7b0a8c3afb455c4e81f47848f93992c
João Gabriel Godoy <joao.sgodoy@hotmail.com>
2025-01-12 22:15:47 -0300

    feat: create User service

---
+++ src/main/java/com/picpaysimplificado/services/UserService.java
@@ -0,0 +1,33 @@
+package com.picpaysimplificado.services;
+
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        if (sender.getUserType() == UserType.MERCHANT) {
+            throw new Exception("Usuário do tipo lojista não está autorizado a realizar transação");
+        }
+
+        if (sender.getBalance().compareTo(amount) < 0) {
+            throw new Exception("Usuário não tem saldo suficiente");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return this.userRepository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
+    }
+
+    public void saveUser(User user) {
+        this.userRepository.save(user);
+    }
+}
